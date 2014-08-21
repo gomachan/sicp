f5b54556200a5e915d86cf3c1d4933a95059b581
gomachan <go_nakanishi@voyagegroup.com>
2014-08-21 20:47:46 +0900

    問題1.3

---
+++ 1_3.rkt
@@ -0,0 +1,12 @@
+#lang planet neil/sicp
+
+(define (square x) (* x x))
+(define (sum-of-squares x y)
+    (+ (square x) (square y)))
+
+(define (problem-1-3 x y z)
+    (cond ((and (< x y) (< x z)) (sum-of-squares y z))
+          ((and (< y x) (< y z)) (sum-of-squares x z))
+          (else (sum-of-squares x y)) ))
+
+(problem-1-3 3 3 3)

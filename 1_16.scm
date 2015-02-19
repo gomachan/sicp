eb5f77cccf0ee4b26bed52ea2694fe895e014429
gomachan <go_nakanishi@voyagegroup.com>
2015-02-19 21:08:01 +0900

    1_16解いた

--- 1_16.scm
+++ 1_16.scm
@@ -1,3 +1,4 @@
+(use gauche.test)
 (use slib)
 (require `trace)
 
@@ -33,3 +34,23 @@
 ; @see http://temping-amagramer.blogspot.jp/2008/06/schememodulo-remainder.html
 ; moduloは２つの数の符号が異なる場合は正数の方へよった挙動をする
 ; remainderはその逆。
+;
+
+
+; 状態の移り変わりに不変量を定義する技法は反復的アルゴリズムの設計に有用！
+; 状態の移り変わりでa*b^nが常に一定(不変)であるように状態遷移を定義する
+;
+
+(define (fast-expt-iterative b n)
+  (fast-expt-iter b n 1)
+)
+
+(define (fast-expt-iter b n a)
+  (cond ((= n 0) a)
+    ((even? n) (fast-expt-iter (* b b) (/ n 2) a))
+    (else (fast-expt-iter b (- n 1) (* a b)))))
+
+(test-start "fast-expt-iter")
+
+(test "2の4乗は16" 16 (lambda() (fast-expt-iterative 2 4)))
+(test "0の0乗は1" 1 (lambda() (fast-expt-iterative 0 0)))
